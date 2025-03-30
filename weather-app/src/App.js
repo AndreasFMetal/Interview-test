95dbe9c66fb756cebfddab9f7555b672adadb50b
AndreasFMetal <touliosandreas@gmail.com>
2025-03-30 17:26:58 +0100

    initial empty app

---
+++ weather-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React, {useState} from 'react'
+import axios from 'axios'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
